a67e1e30c51ed1c577cb7f247b7bbca384744537
Myk Taylor <myk002@yahoo.com>
2024-12-29 16:29:57 -0800

    prep for initial version of gui/rename

--- docs/gui/rename.rst
+++ docs/gui/rename.rst
@@ -2,28 +2,74 @@ gui/rename
 ==========
 
 .. dfhack-tool::
-    :summary: Give buildings and units new names, optionally with special chars.
-    :tags: unavailable
+    :summary: Modify the name of anything that is nameable.
+    :tags: adventure fort productivity animals items units
 
-Once you select a target on the game map, this tool allows you to rename it. It
-is more powerful than the in-game rename functionality since it allows you to
-use special characters (like diamond symbols), and it also allows you to rename
-enemies and overwrite animal species strings.
-
-This tool supports renaming units, zones, stockpiles, workshops, furnaces,
-traps, and siege engines.
+Once you select a target (by clicking on the game map, by passing a commandline
+parameter, or by using the provided selection widget) this tool allows you
+change its language name, generate a new random name, or rename it with your
+preferred component words. It provides an interface similar to the in-game
+naming panel that you can use to customize your fortress name at embark. That
+is, it allows you to choose words from an in-game language to assemble a name,
+just like the default names that the game generates. You will be able to assign
+units new given and last names. You can also use this tool to set freeform
+"nicknames" for targets that support it.
 
 Usage
 -----
 
+::
+
+    gui/rename [<options>]
+
+Examples
+--------
+
 ``gui/rename``
-    Renames the selected building, zone, or unit.
-``gui/rename unit-profession``
-    Set the unit profession or the animal species string.
+    Load the selected artifact, location, or unit for renaming. If nothing is
+    selected, you can select a target from a list.
+``gui/rename -u 123 --no-target-selector``
+    Load the unit with id ``123`` for renaming and remove the widget that
+    allows selecting a different target.
+``gui/rename --location 2 --site 456``
+    Load the location with "abstract building" ID ``2`` attached to the site
+    with id ``456`` for renaming.
 
-Screenshots
------------
+Options
+-------
 
-.. image:: /docs/images/rename-bld.png
+``-a``, ``--artifact <id>``
+    Rename the artifact with the given item ID.
+``-e``, ``--entity <id>``
+    Rename the historical entity (e.g. site government, world religion, etc)
+    with the given ID.
+``-f``, ``--histfig <id>``
+    Rename the historical figure with the given ID.
+``-l``, ``--location <id>``
+    Rename the location (e.g. tavern, hospital, guildhall, temple) with the
+    given ID. If this option is used, ``--site`` can be specified to indicate
+    locations attached to a specific site. If ``--site`` is not specified, the
+    location will be loaded from the current site.
+``-q``, ``--squad <id>``
+    Rename the squad with the given ID.
+``-s``, ``--site <id>``
+    Rename the site with the given ID.
+``-u``, ``--unit <id>``
+    Rename the unit with the given ID. Renaming a unit also renames the
+    associated historical figure.
+``-w``, ``--world``
+    Rename the current world.
+``--no-target-selector``
+    Do not allow the player to switch naming targets. An option that sets the
+    initial target is required when using this option.
 
-.. image:: /docs/images/rename-prof.png
+Overlays
+--------
+
+This tool supports the following overlays:
+
+``gui/rename.embark``
+    Adds widgets to the embark preparation screen for renaming the starting
+    dwarves.
+``gui/rename.world``
+    Adds a widget to the world generation screen for renaming the world.
--- gui/rename.lua
+++ gui/rename.lua
@@ -1,127 +1,169 @@
--- Rename various objects via gui.
---[====[
+local argparse = require('argparse')
+local gui = require('gui')
+local utils = require('utils')
+local widgets = require('gui.widgets')
 
-gui/rename
-==========
-Backed by `rename`, this script allows entering the desired name
-via a simple dialog in the game ui.
+--
+-- Rename
+--
 
-* ``gui/rename [building]`` in :kbd:`q` mode changes the name of a building.
+Rename = defclass(Rename, widgets.Window)
+Rename.ATTRS {
+    frame_title='Rename',
+    frame={w=87, h=30},
+    resizable=true,
+    resize_mid={w=50, h=20},
+}
 
-  .. image:: /docs/images/rename-bld.png
+function Rename:init(info)
+    self.target = info.target
+    self.sync_targets = info.sync_targets or {}
 
-  The selected building must be one of stockpile, workshop, furnace, trap, or siege engine.
-  It is also possible to rename zones from the :kbd:`i` menu.
-
-* ``gui/rename [unit]`` with a unit selected changes the nickname.
-
-  Unlike the built-in interface, this works even on enemies and animals.
-
-* ``gui/rename unit-profession`` changes the selected unit's custom profession name.
-
-  .. image:: /docs/images/rename-prof.png
-
-  Likewise, this can be applied to any unit, and when used on animals it overrides
-  their species string.
-
-The ``building`` or ``unit`` options are automatically assumed when in relevant UI state.
-
-]====]
-local gui = require 'gui'
-local dlg = require 'gui.dialogs'
-local widgets = require 'gui.widgets'
-local plugin = require 'plugins.rename'
-
-local mode = ...
-local focus = dfhack.gui.getCurFocus()
-
-RenameDialog = defclass(RenameDialog, dlg.InputBox)
-function RenameDialog:init(info)
     self:addviews{
         widgets.Label{
-            view_id = 'controls',
-            text = {
-                {key = 'CUSTOM_ALT_C', text = ': Clear, ',
-                    on_activate = function()
-                        self.subviews.edit.text = ''
-                    end},
-                {key = 'CUSTOM_ALT_S', text = ': Special chars',
-                    on_activate = curry(dfhack.run_script, 'gui/cp437-table')},
+            text={
+                self.target and dfhack.TranslateName(self.target) or 'No target', NEWLINE,
+                self.target and dfhack.TranslateName(self.target, true),
             },
-            frame = {b = 0, l = 0, r = 0, w = 70},
+            auto_width=true,
+        },
+    }
+end
+
+--
+-- RenameScreen
+--
+
+RenameScreen = defclass(RenameScreen, gui.ZScreen)
+RenameScreen.ATTRS {
+    focus_path='rename',
+}
+
+function RenameScreen:init(info)
+    self:addviews{
+        Rename{
+            target=info.target,
+            sync_targets=info.sync_targets,
+            show_selector=info.show_selector,
         }
     }
-    -- calculate text_width once
-    self.subviews.controls:getTextWidth()
 end
 
-function RenameDialog:getWantedFrameSize()
-    local x, y = self.super.getWantedFrameSize(self)
-    x = math.max(x, self.subviews.controls.text_width)
-    return x, y + 2
+function RenameScreen:onDismiss()
+    view = nil
 end
 
-function showRenameDialog(title, text, input, on_input)
-    RenameDialog{
-        frame_title = title,
-        text = text,
-        text_pen = COLOR_GREEN,
-        input = input,
-        on_input = on_input,
-    }:show()
+--
+-- CLI
+--
+
+if not dfhack.isWorldLoaded() then
+    qerror('This script requires a world to be loaded')
 end
 
-local function verify_mode(expected)
-    if mode ~= nil and mode ~= expected then
-        qerror('Invalid UI state for mode '..mode)
+local function get_artifact_target(item)
+    if not item or not item.flags.artifact then return end
+    local gref = dfhack.items.getGeneralRef(item, df.general_ref_type.IS_ARTIFACT)
+    if not gref then return end
+    local rec = df.artifact_record.find(gref.artifact_id)
+    if not rec then return end
+    return rec.name
+end
+
+local function get_unit_target(unit, sync_targets)
+    if not unit then return end
+    local hf = df.historical_figure.find(unit.hist_figure_id)
+    if hf then table.insert(sync_targets, hf.name) end
+    return unit.name
+end
+
+local function get_location_target(site, loc_id)
+    if not site or loc_id < 0 then return end
+    local loc = utils.binsearch(site.buildings, loc_id, 'id')
+    if not loc then return end
+    return loc.name
+end
+
+local function get_target(opts)
+    local target, sync_targets = nil, {}
+    if opts.histfig_id then
+        local hf = df.historical_figure.find(opts.histfig_id)
+        if not hf then qerror('Historical figure not found') end
+        target = hf.name
+        local unit = df.unit.find(hf.unit_id)
+        if unit then table.insert(sync_targets, unit.name) end
+    elseif opts.item_id then
+        target = get_artifact_target(df.item.find(opts.item_id))
+        if not target then qerror('Artifact not found') end
+    elseif opts.location_id then
+        local site = opts.site_id and df.world_site.find(opts.site_id) or dfhack.world.getCurrentSite()
+        if not site then qerror('Site not found') end
+        target = get_location_target(site, opts.location_id)
+        if not target then qerror('Location not found') end
+    elseif opts.site_id then
+        local site = df.world_site.find(opts.site_id)
+        if not site then qerror('Site not found') end
+        target = site.name
+    elseif opts.squad_id then
+        local squad = df.squad.find(opts.squad_id)
+        if not squad then qerror('Squad not found') end
+        target = squad.name
+    elseif opts.unit_id then
+        target = get_unit_target(df.unit.find(opts.unit_id), sync_targets)
+        if not target then qerror('Unit not found') end
+    elseif opts.world then
+        target = df.global.world.world_data.name
+    end
+    return target, sync_targets
+end
+
+local opts = {
+    help=false,
+    entity_id=nil,
+    histfig_id=nil,
+    item_id=nil,
+    location_id=nil,
+    site_id=nil,
+    squad_id=nil,
+    unit_id=nil,
+    world=false,
+    show_selector=true,
+}
+local positionals = argparse.processArgsGetopt({...}, {
+    { 'a', 'artifact', handler=function(optarg) opts.item_id = argparse.nonnegativeInt(optarg, 'artifact') end },
+    { 'e', 'entity', handler=function(optarg) opts.entity_id = argparse.nonnegativeInt(optarg, 'entity') end },
+    { 'f', 'histfig', handler=function(optarg) opts.histfig_id = argparse.nonnegativeInt(optarg, 'histfig') end },
+    { 'h', 'help', handler = function() opts.help = true end },
+    { 'l', 'location', handler=function(optarg) opts.location_id = argparse.nonnegativeInt(optarg, 'location') end },
+    { 'q', 'squad', handler=function(optarg) opts.squad_id = argparse.nonnegativeInt(optarg, 'squad') end },
+    { 's', 'site', handler=function(optarg) opts.site_id = argparse.nonnegativeInt(optarg, 'site') end },
+    { 'u', 'unit', handler=function(optarg) opts.unit_id = argparse.nonnegativeInt(optarg, 'unit') end },
+    { 'w', 'world', handler=function() opts.world = true end },
+    { '', 'no-target-selector', handler=function() opts.show_selector = false end },
+})
+
+if opts.help or positionals[1] == 'help' then
+    print(dfhack.script_help())
+    return
+end
+
+local target, sync_targets = get_target(opts)
+
+if not target then
+    local unit = dfhack.gui.getSelectedUnit(true)
+    local item = dfhack.gui.getSelectedItem(true)
+    local zone = dfhack.gui.getSelectedCivZone(true)
+    if unit then
+        target = get_unit_target(unit, sync_targets)
+    elseif item then
+        target = get_artifact_target(item)
+    elseif zone then
+        target = get_location_target(df.world_site.find(zone.site_id), zone.location_id)
     end
 end
 
-local unit = dfhack.gui.getSelectedUnit(true)
-local building = dfhack.gui.getSelectedBuilding(true)
-
-if building and (not unit or mode == 'building') then
-    verify_mode('building')
-
-    if plugin.canRenameBuilding(building) then
-        showRenameDialog(
-            'Rename Building',
-            'Enter a new name for the building:',
-            building.name,
-            curry(plugin.renameBuilding, building)
-        )
-    else
-        dlg.showMessage(
-            'Rename Building',
-            'Cannot rename this type of building.', COLOR_LIGHTRED
-        )
-    end
-elseif unit then
-    if mode == 'unit-profession' then
-        showRenameDialog(
-            'Rename Unit',
-            'Enter a new profession for the unit:',
-            unit.custom_profession,
-            function(newval)
-                unit.custom_profession = newval
-            end
-        )
-    else
-        verify_mode('unit')
-
-        local vname = dfhack.units.getVisibleName(unit)
-        local vnick = ''
-        if vname and vname.has_name then
-            vnick = vname.nickname
-        end
-
-        showRenameDialog(
-            'Rename Unit',
-            'Enter a new nickname for the unit:',
-            vnick,
-            curry(dfhack.units.setNickname, unit)
-        )
-    end
-elseif mode then
-    verify_mode(nil)
-end
+view = view and view:raise() or RenameScreen{
+    target=target,
+    sync_targets=sync_targets,
+    show_selector=opts.show_selector
+}:show()
