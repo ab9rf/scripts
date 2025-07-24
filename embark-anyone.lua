5295834dc6625da9650ab65cc32ab47318733b55
SilasD <silas.dunsmore@gmail.com>
2025-07-24 07:46:04 -0700

    Test the current viewscreen
    The bug: if the embark-anyone script is executed on any viewscreen other than the embark
    viewscreen, it aborts with a stack trace.
    
    This bugfix makes it cleanly abort with a reasonably-descriptive error message.
    
    This was found while diagnosing Issue #5509, but is not related.
    
    Minimal changes to the script.
    
    Note on the code change: by moving the function addCivToEmbarkList() inside the function
    embarkAnyone(), addCivToEmbarkList() cannot execute unless embarkAnyone() has at least
    passed its safety check.

--- embark-anyone.lua
+++ embark-anyone.lua
@@ -1,19 +1,17 @@
 local dialogs = require('gui.dialogs')
 local utils = require('utils')
 
-function addCivToEmbarkList(info)
-   local viewscreen = dfhack.gui.getDFViewscreen(true)
-   if viewscreen._type ~= df.viewscreen_choose_start_sitest then
-      qerror("This script can only be used on the embark screen!")
+function embarkAnyone()
+
+   function addCivToEmbarkList(info)
+      local viewscreen = dfhack.gui.getDFViewscreen(true)
+
+      viewscreen.start_civ:insert ('#', info.civ)
+      viewscreen.start_civ_nem_num:insert ('#', info.nemeses)
+      viewscreen.start_civ_entpop_num:insert ('#', info.pops)
+      viewscreen.start_civ_site_num:insert ('#', info.sites)
    end
 
-   viewscreen.start_civ:insert ('#', info.civ)
-   viewscreen.start_civ_nem_num:insert ('#', info.nemeses)
-   viewscreen.start_civ_entpop_num:insert ('#', info.pops)
-   viewscreen.start_civ_site_num:insert ('#', info.sites)
-end
-
-function embarkAnyone()
    local viewscreen = dfhack.gui.getDFViewscreen(true)
    if viewscreen._type ~= df.viewscreen_choose_start_sitest then
       qerror("This script can only be used on the embark screen!")
